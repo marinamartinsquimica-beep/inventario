ec1f723f9c9c8c6487ec33d91fbfd4b20077170c
marinamartinsquimica-beep <marinamartinsquimica@gmail.com>
2026-06-27 19:48:41 -0300

    Add files via upload

---
+++ service-worker.js
@@ -0,0 +1,36 @@
+const CACHE_NAME = "inventario-cache-v10";
+
+const FILES_TO_CACHE = [
+  "./",
+  "./inicio.html",
+  "./index.html",
+  "./manifest.json",
+  "./icon-192.png",
+  "./icon-512.png"
+];
+
+self.addEventListener("install", event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(FILES_TO_CACHE))
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener("activate", event => {
+  event.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(
+        keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k))
+      )
+    )
+  );
+  self.clients.claim();
+});
+
+self.addEventListener("fetch", event => {
+  event.respondWith(
+    caches.match(event.request).then(resp => {
+      return resp || fetch(event.request).catch(() => caches.match("./inicio.html"));
+    })
+  );
+});
