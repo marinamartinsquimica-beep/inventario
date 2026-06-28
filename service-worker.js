63d3c562e7e8aa2e44738977a6414c12cbecd016
marinamartinsquimica-beep <marinamartinsquimica@gmail.com>
2026-06-27 21:01:26 -0300

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,7 @@
-const CACHE_NAME = "inventario-cache-v4";
+const CACHE_NAME = "inventario-cache-v5";
+
 const FILES_TO_CACHE = [
+  "./",
   "./home.html",
   "./index.html",
   "./manifest.json",
@@ -8,22 +10,33 @@ const FILES_TO_CACHE = [
   "./splash-2732x2732.png"
 ];
 
+// Instala e adiciona arquivos ao cache
 self.addEventListener("install", event => {
   event.waitUntil(
-    caches.open(CACHE_NAME).then(cache => cache.addAll(FILES_TO_CACHE))
+    caches.open(CACHE_NAME).then(cache => {
+      return cache.addAll(FILES_TO_CACHE);
+    })
   );
   self.skipWaiting();
 });
 
+// Ativa e remove caches antigos
 self.addEventListener("activate", event => {
   event.waitUntil(
-    caches.keys().then(keys =>
-      Promise.all(keys.filter(key => key !== CACHE_NAME).map(key => caches.delete(key)))
-    )
+    caches.keys().then(keys => {
+      return Promise.all(
+        keys.map(key => {
+          if (key !== CACHE_NAME) {
+            return caches.delete(key);
+          }
+        })
+      );
+    })
   );
   self.clients.claim();
 });
 
+// Busca arquivos no cache ou na rede
 self.addEventListener("fetch", event => {
   event.respondWith(
     caches.match(event.request).then(response => {
